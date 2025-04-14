3eacf9adc9c5746b31f5fd8b9efef7a807f63c49
Ahmed Atwa <aatwa86@gmail.com>
2025-04-14 08:20:02 +0100

    added CI Version Constant

--- admin/app/Controllers/Common/Footer.php
+++ admin/app/Controllers/Common/Footer.php
@@ -7,13 +7,14 @@ use CodeIgniter\CodeIgniter;
 
 class Footer extends BaseController
 {
-	public function index()
-	{
-		$data['footer']  = sprintf(lang('common/footer.text_footer'), $this->registry->get('config_name'));
-		$data['version'] = sprintf(lang('common/footer.text_version'), APP_VERSION, CodeIgniter::CI_VERSION);
+    public function index()
+    {
+        $data['footer']  = sprintf(lang('common/footer.text_footer'), $this->registry->get('config_name'));
+        $data['version'] = sprintf(lang('common/footer.text_version'), APP_VERSION, CodeIgniter::CI_VERSION);
 
-		lang('common/footer');
+        lang('common/footer');
 
-		return $this->template->render('common/footer', $data);
-	}
+
+        return $this->template->render('common/footer', $data);
+    }
 }
